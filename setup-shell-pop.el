2fb6b3fd2128ebcefdfa4082858f031af4a4e3ef
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-02 22:56:17 +0800

    Fix shell-pop can't work.
    Code from author's github example.

--- setup-shell-pop.el
+++ setup-shell-pop.el
@@ -1,8 +1,9 @@
 (require 'shell-pop)
+
+(shell-pop-set-universal-key (kbd "<f9>"))
 (shell-pop-set-internal-mode "ansi-term")
 (shell-pop-set-internal-mode-shell "/bin/zsh")
-(shell-pop-set-window-height 45) ;the number for the percentage of the selected window. if 100, shell-pop use the whole of selected window, not spliting.
-(shell-pop-set-window-position "bottom") ;shell-pop-up position. You can choose "top" or "bottom". 
-(global-set-key [f9] 'shell-pop)
+(shell-pop-set-window-height 45)
+(shell-pop-set-window-position "bottom")
 
 (provide 'setup-shell-pop)
