ae3359a2b9cbb5fdf07499bf87f9f4ba489a2f65
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-05 12:27:57 +0800

    Remove not use setting from setup-git.

--- setup-git.el
+++ setup-git.el
@@ -14,7 +14,7 @@
  magit-process-popup-time 10
  magit-completing-read-function 'magit-ido-completing-read)
 
-(global-set-key [(meta f12)] 'magit-status)
+;; (global-set-key [(meta f12)] 'magit-status)
 
 (eval-after-load 'magit
   '(progn
@@ -56,29 +56,29 @@
 
 ;;; git-svn support
 
-(eval-after-load 'magit
-  '(progn
-     (require 'magit-svn)))
+;; (eval-after-load 'magit
+;;   '(progn
+;;      (require 'magit-svn)))
 
-(eval-after-load 'compile
-  '(progn
-     (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
-                         '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
-       (add-to-list 'compilation-error-regexp-alist-alist defn))
-     (dolist (defn '(git-svn-updated git-svn-needs-update))
-       (add-to-list 'compilation-error-regexp-alist defn))))
+;; (eval-after-load 'compile
+;;   '(progn
+;;      (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
+;;                          '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
+;;        (add-to-list 'compilation-error-regexp-alist-alist defn))
+;;      (dolist (defn '(git-svn-updated git-svn-needs-update))
+;;        (add-to-list 'compilation-error-regexp-alist defn))))
 
-(defvar git-svn--available-commands nil "Cached list of git svn subcommands")
+;; (defvar git-svn--available-commands nil "Cached list of git svn subcommands")
 
-(defun git-svn (dir)
-  "Run git svn"
-  (interactive "DSelect directory: ")
-  (unless git-svn--available-commands
-    (setq git-svn--available-commands
-          (string-all-matches "^ \\([a-z\\-]+\\) +" (shell-command-to-string "git svn help") 1)))
-  (let* ((default-directory (vc-git-root dir))
-         (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
-    (compile (concat "git svn "
-                     (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
+;; (defun git-svn (dir)
+;;   "Run git svn"
+;;   (interactive "DSelect directory: ")
+;;   (unless git-svn--available-commands
+;;     (setq git-svn--available-commands
+;;           (string-all-matches "^ \\([a-z\\-]+\\) +" (shell-command-to-string "git svn help") 1)))
+;;   (let* ((default-directory (vc-git-root dir))
+;;          (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
+;;     (compile (concat "git svn "
+;;                      (ido-completing-read "git-svn command: " git-svn--available-commands nil t)))))
 
 (provide 'setup-git)
