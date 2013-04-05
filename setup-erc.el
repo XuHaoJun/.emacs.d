b03cf4fac51d18c50f916d1e4e13c69c6b7a2a2d
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-05 10:05:45 +0800

    Add erc some comment for readable.

--- setup-erc.el
+++ setup-erc.el
@@ -1,13 +1,16 @@
 (require 'erc)
 
+;;; Hide spam
+;Because channel too many people,so someone 'join' 'quit' will let you
+;see too more spam.
 ;; Don't spam me bro  
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))  
-
 ;; Don't spam my modeline.  
 (require 'erc-track)  
 (erc-track-mode 1)
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"  
                                 "324" "329" "332" "333" "353" "477"))
+;;; End of Hide spam
 
 ;;; Log save
 (erc-log-mode)
@@ -15,7 +18,7 @@
       erc-log-channels-directory "~/.emacs.d/.erc.d"
       erc-log-insert-log-on-open nil
       erc-log-file-coding-system 'utf-8)
-;; Auto-saving log files on channel activity
+;; auto-saving log files on channel activity
 (setq erc-save-buffer-on-part nil
       erc-save-queries-on-quit nil
       erc-log-write-after-send t
