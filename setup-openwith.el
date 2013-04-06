adced85d6d3b4d7806d3043d7145af4e118b8cb0
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-06 19:38:31 +0800

    Add more openwith.el extern program rules.

--- setup-openwith.el
+++ setup-openwith.el
@@ -1,9 +1,29 @@
 (require 'openwith)
 
-(setq openwith-associations '(("\\.pdf\\'" "zathura" (file))
-                              ("\\.mov\\|\\.RM$\\|\\.RMVB$\\|\\.avi$\\|\\.AVI$\\|\\.flv$\\|\\.mp3\\|.mp4\\|\\.mkv$\\|\\.rmvb$" "mplayer" (file))
-                              ("\\.CHM$\\|\\.chm$" "chmsee"  (file) )
-                               ))
+(setq openwith-associations
+            (list
+             (list (openwith-make-extension-regexp
+                    '("mpg" "mpeg" "mp3" "mp4"
+                      "avi" "wmv" "wav" "mov" "flv"
+                      "ogm" "ogg" "mkv"))
+                   "smplayer"
+                   '(file))
+             ;; (list (openwith-make-extension-regexp
+             ;;        '("xbm" "pbm" "pgm" "ppm" "pnm"
+             ;;          "png" "gif" "bmp" "tif" "jpeg" "jpg"))
+             ;;       "ristretto"
+             ;;       '(file))
+             (list (openwith-make-extension-regexp
+                    '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
+                   "libreoffice"
+                   '(file))
+             '("\\.chm" "chmsee" (file))
+             (list (openwith-make-extension-regexp
+                    '("pdf" "ps" "ps.gz" "dvi"))
+                   "zathura"
+                   '(file))
+             ))
+
 (openwith-mode t)
 
 (provide 'setup-openwith)
