06db5430cc61c911b3c7965ce3449846b20f13b3
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-09 00:15:26 +0800

    Add geiser use racket.

--- setup-scheme-mode.el
+++ setup-scheme-mode.el
@@ -1,6 +1,7 @@
 (require 'scheme)
 
-(require 'geiser)
+(require 'geiser)	
+(setq geiser-active-implementations '(racket))
 ;;some key binding confilct,some i choice geise to main.
 (eval-after-load 'geiser '(require 'quack))
 
