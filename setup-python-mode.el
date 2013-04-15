77a4ad94c7324007e4332b23ba37bf7065ff93d4
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-15 10:15:28 +0800

    Change jedi use python3.3.
    Because i use archlinux and default /usr/bin/python is 3.3 version,
    but default jedi not support python3.3,so you need install following:
    https://github.com/Astrac/jedi/tree/python-3.3
    
    It can work with python2.7 and 3.3.

--- setup-python-mode.el
+++ setup-python-mode.el
@@ -1,13 +1,13 @@
 (require 'python)
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (setq auto-mode-alist (append '(("/*.\.py$" . python-mode)) auto-mode-alist))
-(setq python-python-command "/usr/bin/python2")
-(setq-default py-indent-offset 2)
+(setq python-python-command "/usr/bin/python")
+;; (setq-default py-indent-offset 2)
 
 (require 'jedi)
 (autoload 'jedi:setup "jedi" nil t)
 (setq jedi:server-command
-      (list "/usr/bin/python2" jedi:server-script))
+      (list "/usr/bin/python" jedi:server-script))
 (setq jedi:complete-on-dot t)
 (add-hook 'python-mode-hook 'jedi:setup)
 ;; (add-hook 'python-mode-hook 'jedi:ac-setup)
