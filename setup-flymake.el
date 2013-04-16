6c9584115b52e48724ebaa618c84901b165b2c3c
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-16 11:58:02 +0800

    Add new flymake version setup.
    Install following:
    https://github.com/illusori/emacs-flymake

--- setup-flymake.el
+++ setup-flymake.el
@@ -11,4 +11,12 @@
 ;; more let check after save(C-x C-s).
 (setq flymake-no-changes-timeout 3600)
 
+;;;https://github.com/illusori/emacs-flymake
+;; Nope, I want my copies in the system temp dir.
+(setq flymake-run-in-place nil)
+;; This lets me say where my temp dir is.
+(setq temporary-file-directory "~/.emacs.d/tmp/")
+;; I want to see all errors for the line.
+(setq flymake-number-of-errors-to-display nil)
+
 (provide 'setup-flymake)
