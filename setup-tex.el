ae24b86582ba061c6a2084a9428f130d1453ff8e
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-16 21:31:43 +0800

    Tidy setup-tex.el let column not bigger 80.

--- setup-tex.el
+++ setup-tex.el
@@ -5,15 +5,16 @@
 (setq-default TeX-master nil)
 
 ;; choice other soft open it.
-(setq TeX-view-program-list '(("pxdvi" "pxdvi -watchfile 1 %d")
-                              ("TeXworks" "texworks %o")
-                              ("fwdevince" "fwdevince %o %n \"%b\"")
-                              ("Okular" "okular --unique \"file:%o#src:%n `pwd`/%b\"")
-                              ("zathura" "zathura -s -x \"emacsclient --no-wait +%%{line} %%{input}\" %o")
-                              ("qpdfview" "qpdfview --unique \"%o#src:%b:%n:0\"")
-                              ("PdfViewer" "pdfviewer \"file:%o#src:%n %b\"")
-                              ("acroread" "acroread %o")
-                              ("pdfopen" "pdfopen -viewer ar9-tab %o")))
+(setq TeX-view-program-list
+      '(("pxdvi" "pxdvi -watchfile 1 %d")
+        ("TeXworks" "texworks %o")
+        ("fwdevince" "fwdevince %o %n \"%b\"")
+        ("Okular" "okular --unique \"file:%o#src:%n `pwd`/%b\"")
+        ("zathura" "zathura -s -x \"emacsclient --no-wait +%%{line} %%{input}\" %o")
+        ("qpdfview" "qpdfview --unique \"%o#src:%b:%n:0\"")
+        ("PdfViewer" "pdfviewer \"file:%o#src:%n %b\"")
+        ("acroread" "acroread %o")
+        ("pdfopen" "pdfopen -viewer ar9-tab %o")))
 (setq TeX-view-program-selection '((output-dvi "pxdvi")
                                    (output-pdf "zathura")))
 
@@ -21,7 +22,6 @@
 (setq preview-image-type 'dvipng)
 (setq TeX-source-correlate-method 'synctex)
 (setq TeX-source-correlate-start-server t)
-(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
 
