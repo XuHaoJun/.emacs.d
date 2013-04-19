bdf39c70de1e32c72ff1886078260f8021a122cd
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-20 02:19:13 +0800

    Enable multi-term keybinding.

--- setup-multi-term.el
+++ setup-multi-term.el
@@ -4,8 +4,6 @@
 (setq multi-term-dedicated-select-after-open-p t)
 (setq multi-term-dedicated-window-height 17)
 (setq multi-term-dedicated-max-window-height 25)
-(setq term-bind-key-alist nil)
-(setq term-unbind-key-list nil)
 
 (global-set-key (kbd "C-<f9>") 'multi-term-dedicated-toggle)
 (global-set-key (kbd "M-<f9>") 'multi-term)
