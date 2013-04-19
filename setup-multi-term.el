41a3b5f2ce405d489045442a2923cc39e623f0e0
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-20 03:34:51 +0800

    Add multi-term keybind and work with shell-pop.

--- setup-multi-term.el
+++ setup-multi-term.el
@@ -5,6 +5,15 @@
 (setq multi-term-dedicated-window-height 17)
 (setq multi-term-dedicated-max-window-height 25)
 
+(defun term-send-undo ()
+  "Undo in term mode."
+  (interactive)
+  (term-send-raw-string "\C-_"))
+(setq term-bind-key-alist
+      (append '(("C-c C-s" . term-stop-subjob)
+                ("C-/" . term-send-undo))
+              term-bind-key-alist))
+
 (global-set-key (kbd "C-<f9>") 'multi-term-dedicated-toggle)
 (global-set-key (kbd "M-<f9>") 'multi-term)
 
--- setup-shell-pop.el
+++ setup-shell-pop.el
@@ -1,7 +1,9 @@
 (require 'shell-pop)
 
 (shell-pop-set-universal-key (kbd "<f9>"))
-(shell-pop-set-internal-mode "ansi-term")
+(add-to-list 'shell-pop-internal-mode-list '("multi-term" "*terminal<1>*" '(lambda () (multi-term))))
+(shell-pop-set-internal-mode "multi-term")
+;; (shell-pop-set-internal-mode "ansi-term")
 (shell-pop-set-internal-mode-shell "/bin/zsh")
 (shell-pop-set-window-height 45)
 (shell-pop-set-window-position "bottom")
