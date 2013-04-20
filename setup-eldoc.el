7a1bfd1147851855f21910f1468980a3be2abe7f
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-20 14:17:50 +0800

    Change eldoc setup.

--- setup-eldoc.el
+++ setup-eldoc.el
@@ -1,5 +1,6 @@
-(setq eldoc-idle-delay 0)
-(autoload 'turn-on-eldoc-mode "eldoc" nil t)
+(require 'eldoc nil t)
+(setq eldoc-idle-delay 0.2)
+(setq eldoc-echo-area-use-multiline-p t)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 
