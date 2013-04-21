8d3fa776e77563553bab63b94063d0c5875a8511
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-22 01:33:29 +0800

    Modularize setup-evil.el code.

---
+++ setup-evil-basic.el
@@ -0,0 +1,70 @@
+(require 'evil)
+
+(setq evil-default-cursor t)
+(setq evil-auto-indent t)
+(setq evil-find-skip-newlines t)
+(setq evil-move-cursor-back nil)
+(setq evil-cross-lines t)
+(setq evil-mode-line-format nil)
+
+(evil-set-initial-state 'term-mode 'emacs)
+(evil-set-initial-state 'ansi-term 'emacs)
+(evil-set-initial-state 'dired-mode 'emacs)
+
+;; Modified version of evil-ret that moves the cursor to the first
+;; non-blank on the line, like vim.
+(evil-define-motion rob/evil-ret (count)
+  "Move the cursor COUNT lines down.
+If point is on a widget or a button, click on it.
+In Insert state, insert a newline."
+  :type line
+  (let* ((field  (get-char-property (point) 'field))
+         (button (get-char-property (point) 'button))
+         (doc    (get-char-property (point) 'widget-doc))
+         (widget (or field button doc)))
+    (cond
+     ((and widget
+           (fboundp 'widget-type)
+           (fboundp 'widget-button-press)
+           (or (and (symbolp widget)
+                    (get widget 'widget-type))
+               (and (consp widget)
+                    (get (widget-type widget) 'widget-type))))
+      (when (evil-operator-state-p)
+        (setq evil-inhibit-operator t))
+      (when (fboundp 'widget-button-press)
+        (widget-button-press (point))))
+     ((and (fboundp 'button-at)
+           (fboundp 'push-button)
+           (button-at (point)))
+      (when (evil-operator-state-p)
+        (setq evil-inhibit-operator t))
+      (push-button))
+     ((or (evil-emacs-state-p)
+          (and (evil-insert-state-p)
+               (not buffer-read-only)))
+      (if (not evil-auto-indent)
+          (newline count)
+        (delete-horizontal-space t)
+        (newline count)
+        (indent-according-to-mode)))
+     (t
+      (evil-next-line count)
+      (evil-first-non-blank)))))
+
+(define-key evil-motion-state-map (kbd "RET") 'rob/evil-ret)
+
+;; Emacs key 
+(define-key evil-normal-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-insert-state-map "\C-e" 'evil-end-of-line)
+
+;; Evil key
+(evil-define-operator evil-yank-line-end (beg end type register)
+  "Yank to end of line."
+  :motion evil-end-of-line
+  (interactive "<R><x>")
+  (evil-yank beg end type register))
+(define-key evil-normal-state-map "Y" 'evil-yank-line-end)
+
+(provide 'setup-evil-basic)
---
+++ setup-evil-leader.el
@@ -0,0 +1,36 @@
+(require 'evil)
+(require 'evil-leader)
+
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "," 'smex
+  "#" 'rgr/ido-erc-buffer
+  "%" 'ido-term-buffer
+  "<" 'winner-undo
+  ">" 'winner-redo
+  "0" 'delete-window
+  "1" 'delete-other-windows
+  "2" 'split-window-below
+  "3" 'split-window-right
+  "b" 'ido-switch-buffer
+  "B" 'ibuffer
+  "c" 'recompile
+  "C" 'compile
+  "d" 'ido-dired
+  "e" 'er/expand-region
+  "f" 'ido-find-file
+  "F" 'recentf-ido-find-file
+  "g" 'magit-status
+  "G" 'magit-log
+  "i" 'flex-isearch-forward
+  "I" 'flex-isearch-backward
+  "k" 'kill-buffer
+  "K" 'kill-buffer-and-window
+  "r" 'vr/replace
+  "R" 'vr/query-replace
+  "s" 'save-buffer
+  "S" 'save-some-buffers
+  "t" 'gtags-find-tag
+  "T" 'gtags-find-symbol)
+
+(provide 'setup-evil-leader)
