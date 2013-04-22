2979131b65582dd0bca10030e63abbc83a39cd1e
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-22 15:22:31 +0800

    Add android-mode my keybinding.

--- setup-android-mode.el
+++ setup-android-mode.el
@@ -1,7 +1,22 @@
 (require 'android-mode)
 
+;; it like ant debug && ant installd && adb shell am start ....
 (defun android-ant-my ()
   (interactive)
-  (android-ant "debug && ant installd"))
+  (let* ((package (android-project-package))
+         (current (android-current-buffer-class-name))
+         (activity (if (member current
+                               (android-project-main-activities))
+                       current
+                     (car (android-project-main-activities "LAUNCHER"))))
+         (command (concat (android-tool-path "adb")
+                          " shell am start -n "
+                          (concat package "/" activity))))
+    (android-ant (concat "debug && ant installd && "
+                         command))))
+
+(define-key android-mode-map
+  (read-kbd-macro (concat android-mode-key-prefix " " "m"))
+  'android-ant-my)
 
 (provide 'setup-android-mode)
