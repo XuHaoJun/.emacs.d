cf9f97bb49459ad23eea3d9e638519942e9b4b66
XuHaoJun <XuHaoJunTW@gmail.com>
2013-04-28 14:42:13 +0800

    Add "C-x B" is helm-mini.

--- setup-helm.el
+++ setup-helm.el
@@ -3,6 +3,8 @@
 (require 'helm-ag)
 ;; (helm-mode 1)
 
+(global-set-key (kbd "C-x B") 'helm-mini)
+
 (require 'helm-c-moccur)
 (global-set-key (kbd "M-o") 'helm-c-moccur-occur-by-moccur)
 (global-set-key (kbd "C-M-o") 'helm-c-moccur-dmoccur)
