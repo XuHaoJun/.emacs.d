16c890981569ecf0f8d76e68e9af0550f15575e7
XuHaoJun <XuHaoJunTW@gmail.com>
2013-05-07 10:39:03 +0800

    Tweak flymake not check at open newline.

--- setup-flymake-basic.el
+++ setup-flymake-basic.el
@@ -2,14 +2,15 @@
 
 ;;it displays any flymake error for the current line on the minibuffer.
 (eval-after-load 'flymake '(require 'flymake-cursor))
-;;The built-in package help-at-pt provides similar functionality, and also doesn’t clobber the minibuffer text. 
-  (custom-set-variables
-     '(help-at-pt-timer-delay 0.9)
-     '(help-at-pt-display-when-idle '(flymake-overlay)))
+;;The built-in package help-at-pt provides similar functionality, and also doesn’t clobber the minibuffer text.
+(custom-set-variables
+ '(help-at-pt-timer-delay 0.9)
+ '(help-at-pt-display-when-idle '(flymake-overlay)))
 
 ;; Only run flymake if I've not been typing for 60 seconds
 ;; more let check after save(C-x C-s).
 (setq flymake-no-changes-timeout 3600)
+(setq flymake-start-syntax-check-on-newline nil)
 
 ;;;https://github.com/illusori/emacs-flymake
 ;; Nope, I want my copies in the system temp dir.
