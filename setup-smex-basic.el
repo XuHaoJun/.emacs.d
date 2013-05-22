43d4c757b1806419e19b153f4bcb635547647e17
XuHaoJun <XuHaoJunTW@gmail.com>
2013-05-23 00:36:52 +0800

    Move semx cache file to emacs dir.

--- setup-smex-basic.el
+++ setup-smex-basic.el
@@ -1,5 +1,7 @@
 (require 'smex)
 
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+
 ;; Smart M-x
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
