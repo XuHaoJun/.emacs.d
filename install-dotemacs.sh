84da297ad08ddf26180c6bfb88f7e83cb1390f86
XuHaoJun <XuHaoJunTW@gmail.com>
2013-06-04 13:41:44 +0800

    Add install-dotemacs.sh for intstall.

---
+++ install-dotemacs.sh
@@ -0,0 +1,61 @@
+#!/bin/zsh
+
+# Global var
+dotemacs_dir="$HOME/.emacs.d"
+base_pwd=`pwd`
+
+
+# Util function
+function warn ()
+{
+    echo "$1" >&2
+}
+
+function die ()
+{
+    warn "$1"
+    exit 1
+}
+
+
+# Important function
+function check_dotemacs ()
+{
+    test "[ -e $HOME/{.emacs,.emacs.d} ]" &&
+    die "Faild, because have .emacs.d or .emacs on your home path"
+}
+
+
+function update_self ()
+{
+    git pull
+    git submodule update --init
+}
+
+
+function creat_dotemacs ()
+{
+    mkdir $dotemacs_dir
+    find .var -type d -exec mkdir -p $dotemacs_dir/{} \;
+}
+
+
+function install_dotemacs ()
+{
+    creat_dotemacs
+
+    ln -s $base_pwd/*.el $dotemacs_dir
+    ln -s $base_pwd/*(/) $dotemacs_dir
+}
+
+
+function main ()
+{
+    check_dotemacs
+
+    update_self
+
+    install_dotemacs
+}
+
+main
