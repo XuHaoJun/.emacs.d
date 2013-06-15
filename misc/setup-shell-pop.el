05ebd0730296d67ac9e70acbd2079c4656c35e88
XuHaoJun <XuHaoJunTW@gmail.com>
2013-06-15 19:38:48 +0800

    Fix shell-pop setting can't work.
    Because shell-pop update to lasted version, it change to use customize-group.

--- misc/setup-shell-pop.el
+++ misc/setup-shell-pop.el
@@ -1,11 +1,13 @@
 (require 'shell-pop)
 
-(shell-pop-set-universal-key (kbd "<f9>"))
-(add-to-list 'shell-pop-internal-mode-list '("multi-term" "*terminal<1>*" '(lambda () (multi-term))))
-(shell-pop-set-internal-mode "multi-term")
-;; (shell-pop-set-internal-mode "ansi-term")
-(shell-pop-set-internal-mode-shell "/bin/zsh")
-(shell-pop-set-window-height 45)
-(shell-pop-set-window-position "bottom")
+(custom-set-variables
+ '(shell-pop-set-internal-mode "multi-term")
+ '(shell-pop-set-internal-mode-shell "/bin/zsh")
+ '(shell-pop-set-window-height 45)
+ '(shell-pop-set-window-position "bottom")
+ '(shell-pop-shell-type (quote ("multi-term" "*terminal<1>*" (lambda nil (multi-term)))))
+ '(shell-pop-term-shell "/bin/zsh")
+ '(shell-pop-universal-key "<f9>")
+ '(shell-pop-window-height 45))
 
 (provide 'setup-shell-pop)
