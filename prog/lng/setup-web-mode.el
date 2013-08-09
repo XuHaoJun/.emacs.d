eba6b6c0fc562d0c3d1fadcbd4a89111f44a2530
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-10 01:26:24 +0800

    Fix emmet-mode can't match web-mode indent-offset.

--- prog/lng/setup-web-mode.el
+++ prog/lng/setup-web-mode.el
@@ -1,5 +1,6 @@
-;; depend on evil and emmet
+;;; depend on evil and emmet
 (require 'web-mode)
+
 ;; (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
@@ -7,7 +8,12 @@
 ;; (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 
+
+(require 'emmet-mode)
 (add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'emmet-mode-hook
+          (lambda ()
+            (setq emmet-indentation web-mode-markup-indent-offset)))
 
 (add-hook 'web-mode-hook
           (lambda ()
@@ -15,6 +21,8 @@
               'web-mode-tag-match)
             (define-key evil-insert-state-local-map (kbd "<tab>")
               'emmet-expand-yas)))
+
 ;; (add-to-list 'ac-modes 'web-mode)
+
 
 (provide 'setup-web-mode)
