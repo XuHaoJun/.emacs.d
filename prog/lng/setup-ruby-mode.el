9ccafeb8cae03653640b0b6f4992c7b0840995b9
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-13 01:28:07 +0800

    Check extern program rsense when use it.

--- prog/lng/setup-ruby-mode.el
+++ prog/lng/setup-ruby-mode.el
@@ -1,14 +1,14 @@
-
 ;; Let emacs find my local gem in my home.
 (setenv "GEM_HOME" (expand-file-name "~/.gem"))
 
 ;; Depend on extern program rsense.
-(setq rsense-home "/opt/rsense-0.3")
-(require 'rsense)
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (add-to-list 'ac-sources 'ac-source-rsense-method)
-            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+(when (executable-find "rsense")
+  (setq rsense-home "/opt/rsense-0.3")
+  (require 'rsense)
+  (add-hook 'ruby-mode-hook
+            (lambda ()
+              (add-to-list 'ac-sources 'ac-source-rsense-method)
+              (add-to-list 'ac-sources 'ac-source-rsense-constant))))
 
 (require 'yari)
 
