e670059aff5dfd39c7bc2f5d2d8dbb6dcc5277fc
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-12 18:24:11 +0800

    Tidy setup-evil-leader.el.

--- edit/evil/setup-evil-leader.el
+++ edit/evil/setup-evil-leader.el
@@ -4,41 +4,41 @@
 
 (evil-leader/set-leader ",")
 (evil-leader/set-key
-  "," 'smex
-  "#" 'wg/ido-erc-buffer
-  "%" 'wg/ido-term-buffer
+  ","  'smex
+  "#"  'wg/ido-erc-buffer
+  "%"  'wg/ido-term-buffer
   "ww" 'wg/ido-for-all-mode
   "wi" 'wg/ido-for-this-mode
-  "<" 'winner-undo
-  ">" 'winner-redo
-  "+" 'balance-windows
-  "=" 'indent-whole-buffer
-  "0" 'delete-window
-  "1" 'delete-other-windows
-  "2" 'split-window-below
-  "3" 'split-window-right
-  "b" 'ido-switch-buffer
-  "B" 'ibuffer
-  "c" 'recompile
-  "C" 'compile
-  "d" 'ido-dired
-  "e" 'er/expand-region
-  "E" 'er/contract-region
-  "f" 'ido-find-file
-  "F" 'recentf-ido-find-file
-  "g" 'magit-status
+  "<"  'winner-undo
+  ">"  'winner-redo
+  "+"  'balance-windows
+  "="  'indent-whole-buffer
+  "0"  'delete-window
+  "1"  'delete-other-windows
+  "2"  'split-window-below
+  "3"  'split-window-right
+  "b"  'ido-switch-buffer
+  "B"  'ibuffer
+  "c"  'recompile
+  "C"  'compile
+  "d"  'ido-dired
+  "e"  'er/expand-region
+  "E"  'er/contract-region
+  "f"  'ido-find-file
+  "F"  'recentf-ido-find-file
+  "g"  'magit-status
   "hg" 'helm-do-grep
   "hf" 'helm-find-files
   "hm" 'helm-c-moccur-occur-by-moccur
-  "i" 'indent-on-window
-  "G" 'magit-log
-  "j" 'ace-jump-buffer
-  "k" 'kill-buffer
-  "K" 'kill-buffer-and-window
-  "r" 'vr/replace
-  "R" 'vr/query-replace
-  "s" 'save-buffer
-  "S" 'save-some-buffers)
+  "i"  'indent-on-window
+  "G"  'magit-log
+  "j"  'ace-jump-buffer
+  "k"  'kill-buffer
+  "K"  'kill-buffer-and-window
+  "r"  'vr/replace
+  "R"  'vr/query-replace
+  "s"  'save-buffer
+  "S"  'save-some-buffers)
 
 (dolist (my-mode '(c-mode c++-mode))
   (evil-leader/set-key-for-mode my-mode
