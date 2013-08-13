eb854895aaf9feb3231b6cbb6374452260159385
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-14 05:12:29 +0800

    Fix evil-acejump can't use in visual-mode(evil).

--- edit/evil/setup-evil-acejump.el
+++ edit/evil/setup-evil-acejump.el
@@ -1,13 +1,14 @@
-(require 'evil)
-(require 'ace-jump-mode)
-
 ;; AceJump is a nice addition to evil's standard motions.
+
 ;; The following definitions are necessary to define evil motions for ace-jump-mode (version 2).
+
 ;; ace-jump is actually a series of commands which makes handling by evil
 ;; difficult (and with some other things as well), using this macro we let it
 ;; appear as one.
+
 (defmacro evil-enclose-ace-jump (&rest body)
-  `(let ((old-mark (mark)))
+  `(let ((old-mark (mark))
+         (ace-jump-mode-scope 'window))
      (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
      (remove-hook 'post-command-hook #'evil-visual-post-command t)
      (unwind-protect
@@ -41,7 +42,9 @@
   (evil-enclose-ace-jump
    (ace-jump-mode 5)
    (forward-char -1)))
-;; some proposals for binding:
+
+(add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
+ ;; some proposals for binding:
 (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-word-mode)
 (define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-line-mode)
 (define-key evil-motion-state-map (kbd "M-SPC") #'evil-ace-jump-char-mode)
@@ -50,15 +53,15 @@
 (define-key evil-operator-state-map (kbd "C-SPC") #'evil-ace-jump-line-to-mode) ; similar to t
 (define-key evil-operator-state-map (kbd "M-SPC") #'evil-ace-jump-char-mode)
 ;; different jumps for different visual modes
-;; (defadvice evil-visual-line (before spc-for-line-jump activate)
-;;   (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-line-mode))
+(defadvice evil-visual-line (before spc-for-line-jump activate)
+  (define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-line-mode))
 
-;; (defadvice evil-visual-char (before spc-for-char-jump activate)
-;;   (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
+(defadvice evil-visual-char (before spc-for-char-jump activate)
+  (define-key evil-motion-state-map (kbd "M-SPC") #'evil-ace-jump-char-mode))
 
-;; (defadvice evil-visual-block (before spc-for-char-jump activate)
-;;   (define-key evil-visual-state-map (kbd "M-k") 'mark-previous-like-this)
-;;   evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode)
+(defadvice evil-visual-block (before spc-for-char-jump activate)
+  (define-key evil-visual-state-map (kbd "M-k") 'mark-previous-like-this)
+  evil-motion-state-map (kbd "M-SPC") #'evil-ace-jump-char-mode)
 
 
 (provide 'setup-evil-acejump)
