c1c9a3396e43aedd498d4d3b0781752fbf479e0f
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-15 09:52:51 +0800

    Add Rakefile Gemfile use ruby-mode.

--- prog/lng/setup-ruby-mode.el
+++ prog/lng/setup-ruby-mode.el
@@ -1,14 +1,10 @@
+(require 'ruby-mode)
+
+(dolist (file-pattern '("Rakefile\\'" "\\.rake\\'" "Gemfile\\'"))
+  (add-to-list 'auto-mode-alist (cons file-pattern '(. ruby-mode))))
+
 ;; Let emacs find my local gem in my home.
 (setenv "GEM_HOME" (expand-file-name "~/.gem"))
-
-;; Depend on extern program rsense.
-;; (when (executable-find "rsense")
-;;   (setq rsense-home "/opt/rsense-0.3")
-;;   (require 'rsense)
-;;   (add-hook 'ruby-mode-hook
-;;             (lambda ()
-;;               (add-to-list 'ac-sources 'ac-source-rsense-method)
-;;               (add-to-list 'ac-sources 'ac-source-rsense-constant))))
 
 (require 'yari)
 
@@ -31,5 +27,14 @@
   (ruby-dev)
   (inf-ruby))
 
+
+;; Depend on extern program rsense.
+;; (when (executable-find "rsense")
+;;   (setq rsense-home "/opt/rsense-0.3")
+;;   (require 'rsense)
+;;   (add-hook 'ruby-mode-hook
+;;             (lambda ()
+;;               (add-to-list 'ac-sources 'ac-source-rsense-method)
+;;               (add-to-list 'ac-sources 'ac-source-rsense-constant))))
 
 (provide 'setup-ruby-mode)
