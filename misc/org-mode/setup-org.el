236664a51afd256f868846c9b834c6c978f8b210
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-19 09:29:13 +0800

    Fix chinese word have extra space on org to html.
    This is a workaround way.
    More about this is:
    http://stackoverflow.com/questions/8550112/prevent-browser-converting-n-between-lines-into-space-for-chinese-characters/8551033#8551033

--- misc/org-mode/setup-org.el
+++ misc/org-mode/setup-org.el
@@ -13,9 +13,16 @@
 (setq org-octopress-directory-org-posts "~/current/octopress/source/blog")
 (setq org-octopress-setup-file          "~/.emacs.d/misc/org-mode/setupfile-sample.org")
 
-(setq org-emphasis-regexp-components
-      '("       ('\"{" "-       .,:!?;'\")}\\，。" "      
-,\"'" "." 1))
+;; A chinese word problem.
+;; http://stackoverflow.com/questions/8550112/prevent-browser-converting-n-between-lines-into-space-for-chinese-characters/8551033#8551033
+(defun han/org-html-delete-extra-space (string backend info)
+  "Let chinese word to a line if match /han\newline/"
+  (when (and (org-export-derived-backend-p backend 'html)
+             (string-match "\\cc\n" string))
+    (replace-regexp-in-string "\\(\\cc\\)\n\\(\\cc\\)" "\\1\\2" string)))
+(eval-after-load 'ox-html
+  '(add-to-list 'org-export-filter-final-output-functions
+                'han/org-html-delete-extra-space))
 
 ;;From http://wenshanren.org/?p=334
 (defun org-insert-src-block (src-code-type)
