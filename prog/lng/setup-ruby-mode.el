00ba4d4b0fdacb6485d5611b937c28ff77a827bd
XuHaoJun <XuHaoJunTW@gmail.com>
2013-08-29 15:43:59 +0800

    Add ruby-electric can auto indent if-else.

--- prog/lng/setup-ruby-mode.el
+++ prog/lng/setup-ruby-mode.el
@@ -4,13 +4,11 @@
 ;;; evil auto-complete) .
 
 (require 'ruby-mode)
-
+(require 'yari) ; Yet Another RI interface
 (dolist (file-pattern '("Rakefile\\'" "\\.rake\\'" "Gemfile\\'"))
   (add-to-list 'auto-mode-alist (cons file-pattern '(. ruby-mode))))
-
 ;; Let emacs find my local gem in my home.
 (setenv "GEM_HOME" (expand-file-name "~/.gem"))
-
 ;; I bind it on evil-leader see setup-evil-leader.el file
 (defun ruby-send-region-or-block (start end)
   (interactive "r")
@@ -24,7 +22,6 @@
     (let ((end (point)))
       (beginning-of-line)
       (ruby-send-region (point) end))))
-
 ;; Bind on evil
 (add-hook 'ruby-mode-hook
           (lambda ()
@@ -33,20 +30,41 @@
             (define-key evil-motion-state-local-map (kbd "]]") 'ruby-end-of-block)
             (define-key evil-motion-state-local-map (kbd "]]") 'ruby-end-of-block)))
 
-(require 'yari) ; Yet Another RI interface
 
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook 'ruby-electric-mode)
 ;; Bind on evil
 (add-hook 'ruby-mode-hook
           (lambda () (define-key evil-insert-state-local-map (kbd "RET") 'ruby-electric-return)))
+;; Change from source Version: 20130127.1902
+(defun ruby-electric-space (arg)
+  (interactive "P")
+  (self-insert-command (prefix-numeric-value arg))
+  (cond ((ruby-electric-space-can-be-expanded-p)
+         (save-excursion
+           (ruby-indent-line t)
+           (newline)
+           (ruby-electric-insert-end)))
+        ((ruby-electric-space-can-be-indent-p) (ruby-indent-line t))))
+
+(defun ruby-electric-space-can-be-indent-p ()
+  (and (ruby-electric-code-at-point-p)
+       (ruby-electric-matching-word-p)))
+
+(defun ruby-electric-matching-word-p ()
+  (save-excursion
+    (backward-word)
+    (string-match "\\(\\<else\\>\\|\\<elsif\\>\\)" (current-word))))
+
 
 (require 'ruby-block)
 (add-hook 'ruby-mode-hook 'ruby-block-mode)
 
+
 (require 'inf-ruby)
 (setq inf-ruby-implementations
       '(("ruby" . "irb --prompt my-default -r irb/completion")))
+
 
 ;; Use .irbrc and set default's :PROMPT_S => nil,
 ;; then it can work on my robe and correctly output prompt
@@ -57,6 +75,7 @@
   '(add-hook 'robe-mode-hook
              (lambda ()
                (add-to-list 'ac-sources 'ac-source-robe))))
+
 
 ;; ;; ruby #{rsense-home}/etc/config.rb ~/.rsense"
 ;; ;; to generate config file (for use ruby2.0)
