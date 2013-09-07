f3296fd792b40f56d0d86d8d9535633a46f0ee97
XuHaoJun <XuHaoJunTW@gmail.com>
2013-09-08 00:06:09 +0800

    Add show evil state by changing mode-line color.
    Use on terminal.

--- edit/evil/setup-evil-basic.el
+++ edit/evil/setup-evil-basic.el
@@ -69,4 +69,19 @@
 (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
 (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
 
+
+;; change mode-line color by evil state
+(when (not (window-system))
+  (lexical-let ((default-color (cons (face-background 'mode-line)
+                                     (face-foreground 'mode-line))))
+    (add-hook 'post-command-hook
+              (lambda ()
+                (let ((color (cond ((minibufferp) default-color)
+                                   ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                                   ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                                   ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                                   (t default-color))))
+                  (set-face-background 'mode-line (car color))
+                  (set-face-foreground 'mode-line (cdr color)))))))
+
 (provide 'setup-evil-basic)
