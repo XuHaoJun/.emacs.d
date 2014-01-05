75136b086e9b78a8117e6290d8e7a9305b3fc7f0
XuHaoJun <XuHaoJunTW@gmail.com>
2014-01-05 21:06:27 +0800

    Add go-run fun and remove gofmt after save.

--- prog/lng/setup-go-mode.el
+++ prog/lng/setup-go-mode.el
@@ -1,4 +1,5 @@
 (require 'go-autocomplete)
+(require 'go-mode-load)
 
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 
@@ -7,6 +8,13 @@
             (define-key evil-motion-state-local-map "gd" 'godef-jump)
             (define-key evil-motion-state-local-map "gD" 'godef-jump-other-window)))
 
-(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda ()
+                          (setq compilation-finish-functions nil)))
+(defun go-run ()
+  "run current buffer"
+  (interactive)
+  (compile (concat "go run " (buffer-file-name))))
+
+;; (add-hook 'before-save-hook 'gofmt-before-save)
 
 (provide 'setup-go-mode)
