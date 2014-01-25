e37b7dde10d77da6b3f79adede47c017b0a74af4
XuHaoJun <XuHaoJunTW@gmail.com>
2014-01-25 08:17:27 +0800

    Check use external command if find.

--- file-buffer/setup-openwith.el
+++ file-buffer/setup-openwith.el
@@ -1,28 +1,33 @@
 (require 'openwith)
 
 (setq openwith-associations
-            (list
-             (list (openwith-make-extension-regexp
-                    '("mpg" "mpeg" "mp3" "mp4"
-                      "avi" "wmv" "wav" "mov" "flv"
-                      "ogm" "ogg" "mkv" "ogv"))
-                   "smplayer"
-                   '(file))
-             ;; (list (openwith-make-extension-regexp
-             ;;        '("xbm" "pbm" "pgm" "ppm" "pnm"
-             ;;          "png" "gif" "bmp" "tif" "jpeg" "jpg"))
-             ;;       "ristretto"
-             ;;       '(file))
-             (list (openwith-make-extension-regexp
-                    '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
-                   "libreoffice"
-                   '(file))
-             '("\\.chm" "chmsee" (file))
-             (list (openwith-make-extension-regexp
-                    '("pdf" "ps" "ps.gz" "dvi"))
-                   "zathura"
-                   '(file))
-             ))
+      (remove-if nil
+                 (list
+                  (when (executable-find "smplayer")
+                    (list (openwith-make-extension-regexp
+                           '("mpg" "mpeg" "mp3" "mp4"
+                             "avi" "wmv" "wav" "mov" "flv"
+                             "ogm" "ogg" "mkv" "ogv"))
+                          "smplayer"
+                          '(file)))
+                  (when (executable-find "feh")
+                    (list (openwith-make-extension-regexp
+                           '("xbm" "pbm" "pgm" "ppm" "pnm"
+                             "png" "gif" "bmp" "tif" "jpeg" "jpg"))
+                          "feh -FZ"
+                          '(file)))
+                  (when (executable-find "libreoffice")
+                    (list (openwith-make-extension-regexp
+                           '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
+                          "libreoffice"
+                          '(file)))
+                  (when (executable-find "zathura")
+                    (list (openwith-make-extension-regexp
+                           '("pdf" "ps" "ps.gz" "dvi"))
+                          "zathura"
+                          '(file)))
+                  (when (executable-find "chmsee")
+                    '("\\.chm" "chmsee" (file))))))
 
 (openwith-mode t)
 
