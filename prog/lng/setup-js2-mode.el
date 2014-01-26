d680a99f506f07baa1502f03dc782788a1dafc2f
XuHaoJun <XuHaoJunTW@gmail.com>
2014-01-26 08:50:35 +0800

    Add key-binding for javascript source jump.

--- prog/lng/setup-js2-mode.el
+++ prog/lng/setup-js2-mode.el
@@ -1,10 +1,16 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
 (add-hook 'js2-mode-hook (lambda ()
                            (slime-js-minor-mode 1)
                            (ac-js2-mode 1)))
 
-
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+;; To add completions for external libraries add something like this:
+;;
+;; (add-to-list 'ac-js2-external-libraries "path/to/lib/library.js")
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (define-key evil-motion-state-local-map "gd" 'ac-js2-jump-to-definition)))
 
 (provide 'setup-js2-mode)
