206d140a0cb4104932d850900370d053ce1c3d0a
XuHaoJun <XuHaoJunTW@gmail.com>
2014-04-19 20:04:12 +0800

    Back to use auto-complete-clang.

--- prog/auto-complete/setup-auto-complete-clang.el
+++ prog/auto-complete/setup-auto-complete-clang.el
@@ -1,12 +1,12 @@
 (require 'auto-complete-clang)
 (setq clang-completion-suppress-error 't)
-;; 添加c-mode和c++-mode的hook，开启auto-complete的clang扩展  
-(defun wttr/ac-cc-mode-setup ()  
-  ;; (make-local-variable 'ac-auto-start)  
-  ;; (setq ac-auto-start nil)              ;auto complete using clang is CPU sensitive  
-  (setq ac-sources (append '(ac-source-clang) ac-sources)))  
-(add-hook 'c-mode-hook 'wttr/ac-cc-mode-setup)  
-(add-hook 'c++-mode-hook 'wttr/ac-cc-mode-setup)  
+;; 添加c-mode和c++-mode的hook，开启auto-complete的clang扩展
+(defun wttr/ac-cc-mode-setup ()
+  ;; (make-local-variable 'ac-auto-start)
+  ;; (setq ac-auto-start nil)              ;auto complete using clang is CPU sensitive
+  (setq ac-sources (append '(ac-source-clang) ac-sources)))
+(add-hook 'c-mode-hook 'wttr/ac-cc-mode-setup)
+(add-hook 'c++-mode-hook 'wttr/ac-cc-mode-setup)
 
 ;;You can find the /usr/XXX paths from the messages outputted by the command.
 ;; echo "" | g++ -v -x c++ -E -
@@ -14,17 +14,12 @@
       (mapcar(lambda (item)(concat "-I" item))
              (split-string
               "
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.1/../../../../include/c++/4.8.1
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.1/../../../../include/c++/4.8.1/x86_64-unknown-linux-gnu
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.1/../../../../include/c++/4.8.1/backward
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.1/include
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/../../../../include/c++/4.8.2
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/../../../../include/c++/4.8.2/x86_64-unknown-linux-gnu
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/../../../../include/c++/4.8.2/backward
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/include
  /usr/local/include
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.1/include-fixed
- /usr/include
- /usr/include/SDL/SDL.h
- /usr/include/SDL/SDL_image.h
- /usr/include/SDL/SDL_ttf.h
- /usr/include/libfreenect
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/include-fixed
  /usr/include
 ")))
 
--- prog/auto-complete/setup-auto-complete-init.el
+++ prog/auto-complete/setup-auto-complete-init.el
@@ -1,9 +1,9 @@
-(require 'auto-complete)
-
 (require 'setup-auto-complete-basic)
 
 ;; (when (executable-find "clang")
 ;;   (require 'setup-auto-complete-clang-async))
+
+;; (require 'setup-auto-complete-clang)
 
 (eval-after-load 'ielm '(require 'setup-auto-complete-ielm))
 
