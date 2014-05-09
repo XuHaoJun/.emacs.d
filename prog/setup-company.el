8767fab4ac80badf5d66f6fa12af034060e50a8d
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-09 23:25:48 +0800

    Reduce company completion bar length.

--- prog/setup-company.el
+++ prog/setup-company.el
@@ -5,7 +5,7 @@
 (add-hook 'go-mode-hook (lambda ()
                           (set (make-local-variable 'company-backends) '(company-go))))
 
-(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-tooltip-limit 8)                      ; bigger popup window
 (setq company-minimum-prefix-length 1)               ; autocomplete right after '.'
 (setq company-idle-delay 0.3)                         ; decrease delay before autocompletion popup shows
 (setq company-echo-delay 0)                          ; remove annoying blinking
