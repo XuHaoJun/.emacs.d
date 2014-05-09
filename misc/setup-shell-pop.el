b02fd72487b6d1c25c2606157310dfc68be5c552
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-09 23:26:17 +0800

    Use *pop-terminal* name for shell pop.

--- misc/setup-shell-pop.el
+++ misc/setup-shell-pop.el
@@ -5,7 +5,7 @@
  '(shell-pop-set-internal-mode-shell "/bin/zsh")
  '(shell-pop-set-window-height 45)
  '(shell-pop-set-window-position "bottom")
- '(shell-pop-shell-type (quote ("multi-term" "*terminal<1>*" (lambda nil (multi-term)))))
+ '(shell-pop-shell-type (quote ("multi-term" "*pop-terminal*" (lambda nil (multi-term)))))
  '(shell-pop-term-shell "/bin/zsh")
  '(shell-pop-universal-key "<f9>")
  '(shell-pop-window-height 45))
