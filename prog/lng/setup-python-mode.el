9d47470f32761084e795cf50b6bb21ea65fe5583
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-15 00:01:28 +0800

    Disable company-ropemacs backend.
    Only use company-anaconda for python-mode.

--- prog/lng/setup-python-mode.el
+++ prog/lng/setup-python-mode.el
@@ -3,8 +3,11 @@
 ;; need install jedi and epc from pip
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc)
-(eval-after-load 'company-mode 
-                 '(add-to-list 'company-backends 'company-anaconda))
+(eval-after-load 'company-mode
+  '(delete 'company-ropemacs company-backends))
+(eval-after-load 'company-mode
+  '(add-to-list 'company-backends 'company-anaconda))
+
 
 ;; Syntax check by flycheck-mode depend extern flake8 or pylint.
 ;; (add-hook 'python-mode-hook 'flycheck-mode)
