ac71f5fdb15eee77c1bdcf7cb40ff31afc7eaf0a
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-16 19:47:08 +0800

    Only use anaconda for python company-backend.

--- prog/lng/setup-python-mode.el
+++ prog/lng/setup-python-mode.el
@@ -3,11 +3,13 @@
 ;; need install jedi and epc from pip
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc)
-(eval-after-load 'company-mode
-  '(delete 'company-ropemacs company-backends))
-(eval-after-load 'company-mode
-  '(add-to-list 'company-backends 'company-anaconda))
 
+(defun only-use-anaconda ()
+  (add-to-list 'company-backends 'company-anaconda)
+  (delete 'company-ropemacs company-backends))
+
+(eval-after-load 'company-mode
+  '(only-use-anaconda))
 
 ;; Syntax check by flycheck-mode depend extern flake8 or pylint.
 ;; (add-hook 'python-mode-hook 'flycheck-mode)
