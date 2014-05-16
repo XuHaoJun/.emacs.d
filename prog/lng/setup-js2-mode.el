2fc9a435281d18193bf3f9b293596e62702c72e5
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-16 22:01:56 +0800

    Let js2-mode use company-mode.

--- prog/lng/setup-js2-mode.el
+++ prog/lng/setup-js2-mode.el
@@ -1,17 +1,18 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(setq ac-js2-evaluate-calls t)
+(defun setup-js2-mode-hook ()
+  (ac-js2-mode)
+  (auto-complete-mode -1)
+  (skewer-mode)
+  (rainbow-delimiters-mode)
+  (define-key evil-motion-state-local-map "gd" 'ac-js2-jump-to-definition))
 
-(add-hook 'js2-mode-hook (lambda ()
-                           (slime-js-minor-mode 1)
-                           (ac-js2-mode 1)
-                           (rainbow-delimiters-mode 1)))
+(add-hook 'js2-mode-hook 'setup-js2-mode-hook)
 
 ;; To add completions for external libraries add something like this:
 ;;
 ;; (add-to-list 'ac-js2-external-libraries "path/to/lib/library.js")
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (define-key evil-motion-state-local-map "gd" 'ac-js2-jump-to-definition)))
 
 (provide 'setup-js2-mode)
