9e592583471e1e24e8aea2306858e0a5821bcc23
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-17 22:48:27 +0800

    Disable helm on eshell.

--- file-buffer/setup-helm.el
+++ file-buffer/setup-helm.el
@@ -7,26 +7,11 @@
 (setq-default helm-M-x-requires-pattern 0)
 (setq helm-samewindow nil)
 
-(global-set-key (kbd "C-x B") 'helm-mini)
-
 (require 'helm-c-moccur)
 (global-set-key (kbd "M-o") 'helm-c-moccur-occur-by-moccur)
 (global-set-key (kbd "C-M-o") 'helm-c-moccur-dmoccur)
 (add-hook 'dired-mode-hook
           '(lambda ()
              (local-set-key (kbd "O") 'helm-c-moccur-dired-do-moccur-by-moccur)))
-
-(add-hook 'eshell-mode-hook
-          #'(lambda ()
-              (define-key eshell-mode-map
-                [remap eshell-pcomplete]
-                'helm-esh-pcomplete)))
-
-(defun pcomplete/sudo ()
-  (let ((prec (pcomplete-arg 'last -1)))
-    (cond ((string= "sudo" prec)
-           (while (pcomplete-here*
-                   (funcall pcomplete-command-completion-function)
-                   (pcomplete-arg 'last) t))))))
 
 (provide 'setup-helm)
