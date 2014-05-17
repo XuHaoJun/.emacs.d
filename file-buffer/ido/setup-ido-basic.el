e499b6f27c05e7e20fc8b1cfbab1aac39c580bff
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-17 22:37:06 +0800

    Disable use ido-virtual-buffers.

--- file-buffer/ido/setup-ido-basic.el
+++ file-buffer/ido/setup-ido-basic.el
@@ -7,7 +7,7 @@
       ido-use-filename-at-point 'guess
       ido-max-prospects 10
       ido-default-buffer-method 'selected-window   ;; Allow the same buffer to be open in different frames
-      ido-use-virtual-buffers t
+      ido-use-virtual-buffers nil
       ido-save-directory-list-file (concat user-emacs-directory ".ido.last")
       ido-use-faces nil)
 
