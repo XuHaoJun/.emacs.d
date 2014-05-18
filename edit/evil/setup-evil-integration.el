0795862823f8546ac374c85a055accc158d2fc05
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-19 00:13:07 +0800

    Integrate dired-mode with evil-mode.

--- edit/evil/setup-evil-integration.el
+++ edit/evil/setup-evil-integration.el
@@ -8,8 +8,9 @@
      (defvar help-mode-map)
      (evil-make-overriding-map help-mode-map 'normal)))
 
-(evil-set-initial-state 'term-mode       'emacs)
-(evil-set-initial-state 'ansi-term       'emacs)
+(evil-set-initial-state 'term-mode     'emacs)
+(evil-set-initial-state 'ansi-term     'emacs)
+(evil-set-initial-state 'dired-mode    'emacs)
 ;; workaround for fix evil-leader enable on ibuffer-mode and package-menu-mode
 (evil-set-initial-state 'ibuffer-mode    'normal)
 (evil-set-initial-state 'package-menu-mode    'normal)
