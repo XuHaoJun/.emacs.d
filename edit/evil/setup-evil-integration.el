506488b5f707a80d497c1ea1eed2fbdd380a6e73
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-18 21:24:56 +0800

    Fix help-mode evil-yank not working.

--- edit/evil/setup-evil-integration.el
+++ edit/evil/setup-evil-integration.el
@@ -1,13 +1,18 @@
 (eval-after-load 'package
   '(progn
-     ;; use the standard Dired bindings as a base
      (defvar package-menu-mode-map)
      (evil-make-overriding-map package-menu-mode-map 'normal)))
+
+(eval-after-load 'help-mode
+  '(progn
+     (defvar help-mode-map)
+     (evil-make-overriding-map help-mode-map 'normal)))
 
 (evil-set-initial-state 'term-mode       'emacs)
 (evil-set-initial-state 'ansi-term       'emacs)
 ;; workaround for fix evil-leader enable on ibuffer-mode and package-menu-mode
 (evil-set-initial-state 'ibuffer-mode    'normal)
 (evil-set-initial-state 'package-menu-mode    'normal)
+(evil-set-initial-state 'help-mode    'normal)
 
 (provide 'setup-evil-integration)
