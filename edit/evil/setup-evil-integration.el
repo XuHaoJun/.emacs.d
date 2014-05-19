5a6610bc3b8cbfa6b48a66cac7f67815d427ba28
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-19 13:47:25 +0800

    Use space key for evil-acejump in help-mode.

--- edit/evil/setup-evil-integration.el
+++ edit/evil/setup-evil-integration.el
@@ -6,6 +6,9 @@
 (eval-after-load 'help-mode
   '(progn
      (defvar help-mode-map)
+     ;; force unbound space key for evil-ace-jump
+     ;; origin key is scroll-page, i use C-f(evil-mode) replace it
+     (define-key help-mode-map (kbd "SPC") nil)
      (evil-make-overriding-map help-mode-map 'normal)
      (evil-add-hjkl-bindings help-mode-map 'normal
        "h" 'evil-backward-char)))
@@ -13,7 +16,7 @@
 (evil-set-initial-state 'term-mode     'emacs)
 (evil-set-initial-state 'ansi-term     'emacs)
 (evil-set-initial-state 'dired-mode    'emacs)
-;; workaround for fix evil-leader enable on ibuffer-mode and package-menu-mode
+;; workaround for fix evil-leader enable
 (evil-set-initial-state 'ibuffer-mode    'normal)
 (evil-set-initial-state 'package-menu-mode    'normal)
 (evil-set-initial-state 'help-mode    'normal)
