06948eedcf04135ec82230883f5b9f12f5fe37be
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-21 23:59:17 +0800

    Tidy bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -5,10 +5,10 @@ function die() {
     exit 1
 }
 
+# (save-excursion
 enter_path=`pwd`
-git clone https://github.com/XuHaoJun/emacs.d || die "git clone failed"
-
+git clone https://github.com/XuHaoJun/emacs.d || die "Git clone failed"
 cd ./emacs.d
 rake || die "Rake failed"
-
 cd $enter_path
+# )
