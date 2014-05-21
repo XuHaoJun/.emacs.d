ed193223a3b0fd02582b001244ba362d3670212c
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-22 01:23:36 +0800

    Enable melpa-stable.

--- setup-package.el
+++ setup-package.el
@@ -1,20 +1,20 @@
 (require 'package)
 
-(defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
-(defvar gnu '("gnu" . "http://elpa.gnu.org/packages/"))
-(defvar melpa '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-;; Add marmalade to package repos
-(add-to-list 'package-archives marmalade)
-(add-to-list 'package-archives melpa t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
 (unless (and (file-exists-p "~/.emacs.d/elpa/archives/marmalade")
              (file-exists-p "~/.emacs.d/elpa/archives/gnu")
-             (file-exists-p "~/.emacs.d/elpa/archives/melpa"))
+             (file-exists-p "~/.emacs.d/elpa/archives/melpa")
+             (file-exists-p "~/.emacs.d/elpa/archives/melpa-stable"))
   (when (null package-archive-contents)
     (package-refresh-contents)))
 
+(require 'fullframe)
+(fullframe list-packages quit-window)
 
 (provide 'setup-package)
