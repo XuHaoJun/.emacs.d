574a201d4bc117f64572d93350a8f90cd7d31643
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-21 21:48:55 +0800

    Add company-clang and flycheck-clang settings.

--- prog/lng/setup-cc-mode.el
+++ prog/lng/setup-cc-mode.el
@@ -6,17 +6,26 @@
 
 (setq clang-format-style "google")
 
-(defun setup-gtags-hook ()
+(defun setup-ggtags-hook ()
   (ggtags-mode 1)
-  (define-key evil-motion-state-local-map "gd" 'ggtags-find-tag-dwim))
+  (define-key evil-motion-state-local-map "gd" 'ggtags-find-tag-dwim)
+  (define-key evil-motion-state-local-map "gD" 'ggtags-prev-mark))
 
 (when (executable-find "gtags")
   (require 'ggtags)
-  (add-hook 'c-mode-hook 'setup-gtags-hook)
-  (add-hook 'c++-mode-hook 'setup-gtags-hook))
+  (add-hook 'c-mode-hook 'setup-ggtags-hook)
+  (add-hook 'c++-mode-hook 'setup-ggtags-hook))
 
-;;for work with auto-indent mode
+(setq company-clang-insert-arguments nil)
+
+;Edit;for work with auto-indent mode
 (add-hook 'c-mode-common-hook
           (lambda () (setq indent-tabs-mode t)))
+
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++11")))
+
+(add-hook 'c-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c11")))
 
 (provide 'setup-cc-mode)
