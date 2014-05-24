430ec3043a2e0ccc485cd73075931aeddea78cbd
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-24 19:22:47 +0800

    Drop ac-inf-mongo.

--- prog/setup-inf-mongo.el
+++ prog/setup-inf-mongo.el
@@ -1,9 +1,4 @@
 (setq inf-mongo-command
       (executable-find "mongo"))
 
-(require 'ac-inf-mongo)
-(eval-after-load 'auto-complete
-  '(add-to-list 'ac-modes 'inf-mongo-mode))
-(add-hook 'inf-mongo-mode-hook 'ac-inf-mongo-enable)
-
 (provide 'setup-inf-mongo)
