6547f4a36bf6bf903c4b5613c1266c966160aca7
XuHaoJun <XuHaoJunTW@gmail.com>
2015-06-20 21:51:04 +0800

    Drop ido-preview.
    I never used.

--- lisp/file-buffer/ido/setup-ido-plugin.el
+++ lisp/file-buffer/ido/setup-ido-plugin.el
@@ -1,8 +1,6 @@
 ;;; Require package:
-;;; (ido-sort-mtime smex ido-ubiquitous ido-yes-or-no ido-preview
+;;; (ido-sort-mtime smex ido-ubiquitous ido-yes-or-no
 ;;; flx-ido ido-vertical-mode)
-;;; Require site-lisp:
-;;; (ido-preview)
 
 (require 'setup-smex)
 
@@ -11,17 +9,10 @@
 (ido-sort-mtime-mode 1)
 
 (ido-vertical-mode t)
+(setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
 
 ;; Use ido everywhere
 (setq ido-everywhere t)
 (ido-ubiquitous-mode t)
-
-;; Bind ido-preview
-(add-hook 'ido-setup-hook
-  (lambda()
-    (define-key ido-completion-map (kbd "C-M-p") (lookup-key ido-completion-map (kbd "C-p")))
-    (define-key ido-completion-map (kbd "C-M-n") (lookup-key ido-completion-map (kbd "C-n"))) ; currently, this makes nothing. Maybe they'll make C-n key lately.
-    (define-key ido-completion-map (kbd "C-p") 'ido-preview-backward)
-    (define-key ido-completion-map (kbd "C-n") 'ido-preview-forward)))
 
 (provide 'setup-ido-plugin)
