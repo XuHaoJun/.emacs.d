3989a8ebc76524d7239bbc4b10d9dd007392be9f
XuHaoJun <XuHaoJunTW@gmail.com>
2015-07-21 10:46:33 +0800

    Drop melpa stable.

--- lisp/setup-package.el
+++ lisp/setup-package.el
@@ -2,15 +2,13 @@
 
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
 (unless (and (file-exists-p "~/.emacs.d/elpa/archives/marmalade")
              (file-exists-p "~/.emacs.d/elpa/archives/gnu")
-             (file-exists-p "~/.emacs.d/elpa/archives/melpa")
-             (file-exists-p "~/.emacs.d/elpa/archives/melpa-stable"))
+             (file-exists-p "~/.emacs.d/elpa/archives/melpa"))
   (when (null package-archive-contents)
     (package-refresh-contents)))
 
