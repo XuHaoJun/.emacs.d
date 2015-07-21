cc15c425f3908a1bda4c32ec521b9b2ff8ed062a
XuHaoJun <XuHaoJunTW@gmail.com>
2015-07-21 10:47:01 +0800

    Update git config.

--- lisp/prog/setup-git.el
+++ lisp/prog/setup-git.el
@@ -1,12 +1,8 @@
 ;; require package: (magit gitignore-mode gitconfig-mode github-browse-file)
 
 (require 'magit)
-(require 'git-commit-mode)
 (require 'gitignore-mode)
 (require 'gitconfig-mode)
 (require 'github-browse-file)
-
-(eval-after-load 'magit
-  '(diminish 'magit-auto-revert-mode))
 
 (provide 'setup-git)
